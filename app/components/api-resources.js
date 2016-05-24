bfc7d0297ff3248fd7983324cb03f152b13f0549
Darkmile <Darkmile>
2016-05-24 13:14:51 +0200

    Added id to resource model. Creating a new resource increments the id.

--- app/components/api-resources.js
+++ app/components/api-resources.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    nextResourceId: 1,
     actions: {
         createResource() {
             var resource = this.get('store').createRecord('resource', {
+                resId: this.nextResourceId,
                 path: '/path',
                 name: 'New path',
                 description: 'My new path.',
                 variables: '',
                 methods: ''
             });
+            this.set('nextResourceId', this.nextResourceId + 1);
             console.log('done');
         }
     }
--- app/models/resource.js
+++ app/models/resource.js
@@ -2,9 +2,10 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
 export default Model.extend({
+  resId: attr(),
   path: attr('string'),
   name: attr('string'),
-  description('string'),
+  description: attr('string'),
   variables: attr(),
   methods: attr()
 });
