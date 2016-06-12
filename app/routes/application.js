5f68d3b116cf61c5b7a6b9a482ef584d00baae89
Darkmile <Darkmile>
2016-06-12 15:54:11 +0200

    Combined api-resources and api-entities into a new generalized component.

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    //return this.store.findAll('resource');
     return Ember.RSVP.hash({
       resources: this.store.findAll('resource'),
       entities: this.store.findAll('entity')
