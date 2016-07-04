b3d8dd6f1b5d1e5fe4549d599ae24dee557e7e7d
rigvedpatki <rigved.patki@gmail.com>
2016-07-04 15:52:35 +0200

    changes made to api

--- app/routes/apiurl.js
+++ app/routes/apiurl.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  protocols:[
+    "HTTP/1.1",
+    "HTTPS/1.1"
+  ],
   flashMessages: Ember.inject.service(),
   model(params) {
       return this.store.findRecord('apiurl', params.apiurl_id);
