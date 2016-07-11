bc8974a8d924b57f3e66fe1fb90d021aa7675cad
rigvedpatki <rigved.patki@gmail.com>
2016-07-11 16:07:26 +0200

    Last commit

--- app/routes/apiurl.js
+++ app/routes/apiurl.js
@@ -1,19 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  protocols:[
-    "HTTP/1.1",
-    "HTTPS/1.1"
-  ],
   flashMessages: Ember.inject.service(),
   model(params) {
       return this.store.findRecord('apiurl', params.apiurl_id);
   },
   actions: {
+    saveSelection(selection,componenet){
+      this.set('protocol',selection);
+    },
+    generateEndpoint(apiurl){
+        Ember.Logger.log("log: " + apiurl.get('protocol'));
+      var endpoint_url = null;
+        if (apiurl.get('protocol') == "HTTP/1.1"){
+          Ember.Logger.log("log: " + apiurl.id);
+          endpoint_url = "http://"+apiurl.get('domain')+apiurl.get('base_path');
+        }else if (apiurl.get('protocol') == "HTTPS/1.1") {
+          Ember.Logger.log("log: " + apiurl.id);
+          endpoint_url = "http://"+apiurl.get('domain')+apiurl.get('base_path');
+        }
+        apiurl.set('endpoint_url',endpoint_url);
+    },
       save(apiurl) {
           apiurl.save();
           Ember.Logger.log("Data is saved for API URL id: " + apiurl.id);
           Ember.get(this, 'flashMessages').success('Saved!');
       }
+
   }
 });
--- app/routes/apiurls/new.js
+++ app/routes/apiurls/new.js
@@ -4,16 +4,15 @@ export default Ember.Route.extend({
   model() {
     var var_apiurl = this.store.createRecord('apiurl', {
       protocol: '',
-      port: '',
+      port: '443',
       domain: '',
       base_path:'',
       endpoint_url: ''
     });
     var_apiurl.save();
-
     return var_apiurl;
   },
   redirect(model, transition) {
-    this.transitionTo('apiurl', model.id);
+    this.transitionTo('apiurl', model);
   }
 });
